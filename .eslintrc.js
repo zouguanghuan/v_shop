06215f361dd40f266ce8bc1f9acf4d812ae16739
zouguanghuan <zouguanghuan@126.com>
2020-09-15 16:28:29 +0800

    登陆功能完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 将eslint里面小括号前面必须有空格的坚持禁止
+    'space-before-function-paren':0
   }
 }
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 退出功能，根据路由导航守卫，只需删掉token并重定向到login
+    logout () {
+      window.sessionStorage.clear('token')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
