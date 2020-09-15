85a58b5de38ef0b583c8a889ac8efa84728b37eb
zouguanghuan <zouguanghuan@126.com>
2020-09-15 14:19:28 +0800

    add files

---
+++ src/components/Login.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="找不到">
+      </div>
+      <!-- 登陆表单区域 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username"  prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码区域 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" type="password" prefix-icon="el-icon-key"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="success" @click="login" >登陆</el-button>
+          <el-button type="success" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 这是登陆表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单验证
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入的登陆名称', trigger: 'change' }
+        ],
+        password: [
+          { required: true, message: '请输入3到15位密码', trigger: 'change' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      // 返回一个布尔值
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        // 从返回数据中解构出data对象，并重命名为res，需要await 和 async进行处理
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
+        // 通过网络状态吗判断是否登陆成功
+        if (res.meta.status !== 200) return this.$message.error('登陆失败')// return语句执行以后，函数后面的代码不执行
+        this.$message.success('登陆成功')
+        // 将token保存到sessionStorage
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    background-color: #eee;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: -65px;
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
