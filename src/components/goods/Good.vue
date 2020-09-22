35e427914607abe3c6f3a30c9ac731f986dcbc37
zouguanghuan <zouguanghuan@126.com>
2020-09-22 17:19:27 +0800

    商品展示完善

--- src/components/goods/Good.vue
+++ src/components/goods/Good.vue
@@ -5,21 +5,75 @@
       <el-breadcrumb-item><a href="/">商品管理</a></el-breadcrumb-item>
       <el-breadcrumb-item>商品列表</el-breadcrumb-item>
     </el-breadcrumb>
-
-     <!-- button按钮 -->
-     <el-row>
-      <el-button type="primary">添加分类</el-button>
-    </el-row>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- button按钮 -->
+      <el-row>
+        <div style="display: inline-block; width: 324px; margin-right: 15px;">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query">
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList" clearable></el-button>
+          </el-input>
+        </div>
+        <el-button type="primary">添加商品</el-button>
+      </el-row>
+      <el-table :data="goodsList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格（元）" prop="goods_price"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" style="width: 170px">
+          <template slot-scope="scope">
+            {{ scope.row.add_time | dateFormate }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="queryInfo.pagenum"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="queryInfo.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      goodsList: [],
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 5
+      },
+      total: 2
+    }
   },
-  created() { },
-  methods: {}
+  created() {
+    this.getGoodsList()
+  },
+  methods: {
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', { params: this.queryInfo })
+      if (res.meta.status !== 200) {
+        return this.$message.error('error')
+      }
+      this.goodsList = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+    }
+  }
 }
 </script>
 
