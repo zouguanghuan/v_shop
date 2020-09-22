8d57e2017c9e0b7ca1843406a6713ad16c007586
zouguanghuan <zouguanghuan@126.com>
2020-09-22 11:22:47 +0800

    数据可视化之图表

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -5,15 +5,51 @@
       <el-breadcrumb-item><a href="/">数据统计</a></el-breadcrumb-item>
       <el-breadcrumb-item>数据报表</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 卡片区域 -->
+    <el-card>
+      <div id="main" style="width: 731px;height:400px;"></div>
+    </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import _ from 'lodash'
 export default {
   data() {
-    return {}
+    return {
+      option: {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      }
+    }
   },
-  created() { },
+  created() {},
+  async mounted() {
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('error')
+    }
+    const result = _.merge(res.data, this.option)
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  },
   methods: {}
 }
 </script>
