a467774357b97f0697fcdbfee1a3242affc217e5
zouguanghuan <zouguanghuan@126.com>
2020-09-22 09:34:38 +0800

    参数列表展示

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 将eslint里面小括号前面必须有空格的坚持禁止
-    'space-before-function-paren':0
+    'space-before-function-paren': 0
   }
 }
--- src/components/goods/Param.vue
+++ src/components/goods/Param.vue
@@ -25,10 +25,31 @@
         <!-- 添加动态参数 -->
         <el-tab-pane label="动态参数" name="many">
           <el-button type="primary" size="mini" :disabled="isBtnDisable">添加参数</el-button>
+          <!-- 动态参数表格 -->
+          <el-table :data="manyTableData" border stripe>
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+                <el-button type="danger" size="mini" icon="el-icon-delete">delete</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
                 <!-- 添加静态属性 -->
         <el-tab-pane label="静态属性" name="only">
           <el-button type="primary" size="mini" :disabled="isBtnDisable">添加属性</el-button>
+          <el-table :data="manyTableData" border stripe type="expand">
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button type="primary" size="mini" icon="el-icon-edit">编辑x</el-button>
+                <el-button type="danger" size="mini" icon="el-icon-delete">delete</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
 
@@ -47,8 +68,13 @@ export default {
         children: 'children',
         expandTrigger: 'hover'
       },
+      // 各级联选择器的id值数据
       selectedCateKeys: [],
-      activeName: 'many'
+      activeName: 'many',
+      // 动态参数数据
+      manyTableData: [],
+      // 静态属性的数据
+      onlyTableData: []
     }
   },
   created() {
@@ -62,11 +88,29 @@ export default {
       }
       this.cateList = res.data
     },
+    // 级联选择器的触发函数
     async handleChange() {
       if (this.selectedCateKeys.length !== 3) {
         this.selectedCateKeys = []
+        return
       }
-      const { data:res } = await this.$http.get(`categories/${this.cateID}/attributes`)
+      this.getParamsData()
+    },
+    // 添加按钮的触发方法
+    handleClick () {
+      this.getParamsData()
+    },
+    async getParamsData() {
+      const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`, { params: { sel: this.activeName } })
+      if (res.meta.status !== 200) {
+        return this.$message.error('error')
+      }
+      console.log(res.data)
+      if (this.activeName === 'many') {
+        this.manyTableData = res.data
+      } else {
+        this.onlyTableData = res.data
+      }
     }
   },
   computed: {
@@ -76,8 +120,8 @@ export default {
       }
       return false
     },
-    cateID () {
-      if (this.selectedCateKeys === 3) {
+    cateId () {
+      if (this.selectedCateKeys.length === 3) {
         return this.selectedCateKeys[2]
       }
       return null
